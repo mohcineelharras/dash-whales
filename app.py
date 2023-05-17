353bfb30a0d205136daf02eddbd86a561deb7565
mohcineelharras <mohcineelharras@hotmail.com>
2023-05-17 22:46:19 +0200

    init, basic app works

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
+# Load the data from the CSV files
+dataframes = []
+for filename in os.listdir('output'):
+    if filename.endswith('.csv'):
+        df = pd.read_csv(os.path.join('output', filename), sep=';')
+        dataframes.append(df)
+df = pd.concat(dataframes)
+
+
+# Create the Dash app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Define the app layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1('Token Analysis'),
+            dcc.Dropdown(
+                id='token-dropdown',
+                options=[{'label': i, 'value': i} for i in df['tokenSymbol'].unique()],
+                value='MANA'
+            ),
+            # Add more filters here
+        ], width=5),
+        dbc.Col([
+            dcc.Graph(id='token-graph')
+        ], width=7)
+    ])
+])
+
+# Define the callback to update the graph
+@app.callback(
+    Output('token-graph', 'figure'),
+    [Input('token-dropdown', 'value')]
+)
+def update_graph(selected_token):
+    filtered_df = df[df['tokenSymbol'] == selected_token]
+    # filtered_df['timeStamp'] = pd.to_datetime(filtered_df['timeStamp'], unit='s')
+    # filtered_df['value'] = filtered_df['value'].astype(float) / 1e18
+    figure = go.Figure(
+        data=[
+            go.Scatter(
+                x=filtered_df['timeStamp'],
+                y=filtered_df['value'],
+                mode='lines',
+                name='Value over time'
+            )
+        ],
+        layout=go.Layout(
+            title='Token Value Over Time',
+            yaxis=dict(
+                title='Value ('+selected_token+')',  # Change this to 'Value (USD)' if the values are in USD
+            ),
+            showlegend=True,
+            legend=go.layout.Legend(
+                x=0,
+                y=1.0
+            ),
+            margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+        )
+    )
+    return figure
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ scrap_data.py
@@ -0,0 +1,75 @@
+import requests
+import time
+import pandas as pd
+import json
+
+# Etherscan API URL
+url = "https://api.etherscan.io/api"
+
+# Your Etherscan API Key
+api_key = "VVJFE7IG5WSK8P2UJIJN5MST5UBQEZYVYW"
+
+# Load the JSON file into a dictionary
+with open("dict_tokens_addr.json", "r") as file:
+    dict_addresses = json.load(file)
+
+def get_coin_data(tokenSymbol, contractAddr, n):
+
+    # Get the latest block number
+    params = {
+        "module": "proxy",
+        "action": "eth_blockNumber",
+        "apikey": api_key
+    }
+    response = requests.get(url, params=params)
+    latest_block = int(response.json()["result"], 16)
+
+    # Initialize an empty DataFrame to store the transactions
+    df_transactions = pd.DataFrame()
+
+    # Define the number of transactions to retrieve per API call
+    transactions_per_call = 10_000
+
+    # Iterate 'n' times to retrieve 'n' sets of transactions
+    for i in range(n):
+        # Calculate the start and end blocks for each API call
+        start_block = latest_block - (i + 1) * transactions_per_call
+        end_block = latest_block - i * transactions_per_call
+
+        # Make the API request
+        params = {
+            "module": "account",
+            "action": "tokentx",
+            "contractaddress": contractAddr,
+            "startblock": start_block,
+            "endblock": end_block,
+            "sort": "asc",
+            "apikey": api_key
+        }
+        response = requests.get(url, params=params)
+
+        # Get the transactions data
+        transactions = response.json()["result"]
+
+        # Append the transactions to the DataFrame
+        df_transactions = pd.concat([df_transactions, pd.DataFrame(transactions)])
+
+        # Add a delay to avoid hitting the API rate limit
+        time.sleep(1)
+
+    df_transactions['timeStamp'] = pd.to_datetime(df_transactions['timeStamp'], unit='s')
+    df_transactions['value'] = df_transactions['value'].astype(float) / 1e18
+
+    # Save transactions to a CSV file
+    df_transactions.to_csv(f"output/transactions_{tokenSymbol}.csv", sep=";", index=False)
+    print(f"All transactions saved to transactions_{tokenSymbol}_.csv")
+    print()
+
+# Define the number of blocks to retrieve transactions from
+n_blocks = 20000
+n_loop = n_blocks // 10_000
+
+
+for tokenSymbol, contractAddr in dict_addresses.items():
+    print(f"Key: {tokenSymbol}, Value: {contractAddr}")
+    get_coin_data(tokenSymbol, contractAddr, n_loop)
